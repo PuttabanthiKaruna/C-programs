cfc1aa8a703f94e6e87444a8e65870a2c82e7226
Karuna Puttabanthi <karuna27082007@gmail.com>
2026-02-02 11:32:04 +0530

    Create week1.cpp

---
+++ week1.cpp
@@ -0,0 +1,50 @@
+// a. Define a Student class with attributes: rollNumber, name, marks
+#include<iostream>
+using namespace std;
+class student{
+    int r,m,rno,marks;
+    char n1[26],n2[26];
+    public:
+    void s1details()
+    {
+        cout<<"enter student 1 roll no:"<<endl;
+        cin>>rno;
+        cout<<"enter student 1 name:"<<endl;
+        cin>>n1;
+        cout<<"enter student 1 marks:"<<endl;
+        cin>>marks;
+        
+    }
+    void s2details()
+    {
+        cout<<"enter student 2 roll no:"<<endl;
+        cin>>r;
+        cout<<"enter student 2 name:"<<endl;
+        cin>>n2;
+        cout<<"enter student 2 marks:"<<endl;
+        cin>>m;
+    }
+    
+};
+
+int main()
+{
+student c1,c2;
+c1.s1details();
+c2.s2details();
+return 0;
+}
+
+// output
+// enter student 1 roll no:
+// 123
+// enter student 1 name:
+// kavya
+// enter student 1 marks:
+// 99
+// enter student 2 roll no:
+// 124
+// enter student 2 name:
+// priya
+// enter student 2 marks:
+// 98
